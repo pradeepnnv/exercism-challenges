fb1c06c33ac708044f9e8fe88a758abe11a23389
pradeepnnv <pradeepnnv@gmail.com>
2020-12-21 10:10:49 -0600

    leapyear

---
+++ go/leap/leap.go
@@ -0,0 +1,12 @@
+package leap
+
+// IsLeapYear determines if a given year is leap year or not
+func IsLeapYear(year int) bool {
+	if year%4 == 0 {
+		if year%100 == 0 && year%400 != 0 {
+			return false
+		}
+		return true
+	}
+	return false
+}
