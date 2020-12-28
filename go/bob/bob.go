5d814d24898adeced9b554e8eaa36b85f0bbce4b
pradeepnnv <pradeepnnv@gmail.com>
2020-12-28 12:21:46 -0600

    bob version1
    Signed-off-by: pradeepnnv <pradeepnnv@gmail.com>

---
+++ go/bob/bob.go
@@ -0,0 +1,43 @@
+package bob
+
+import (
+	"regexp"
+	"strings"
+)
+
+// Hey returns Bob's response based on the input remark
+func Hey(remark string) string {
+	// var r string
+	if strings.Trim(remark, " ") == "" { //Address Bob without actually saying anything
+		return "Fine. Be that way!"
+	} else if m, _ := regexp.Match("^[A-Z[:punct:] ]+[?]$", []byte(remark)); m { //Yell a Question
+		return "Calm down, I know what I'm doing!"
+	}
+	// else if m, _ := regexp.Match("^[A-Z| |0-9|,]*[!]*$", []byte(remark)); m { //Yell
+	// 	return "Whoa, chill out!"
+	// }
+
+	words := strings.Fields(remark)
+	// for i, w := range words {
+	// 	if m, _ := regexp.Match("^[A-Z]*[?]$", []byte(w)); m { //Yell a Question
+	// 		return "Calm down, I know what I'm doing!"
+	// 	} else if m, _ := regexp.Match("[?]$", []byte(w)); m { //Question
+	// 		return "Sure."
+	// 	} else if m, _ := regexp.Match("^[A-Z]{2,}.*[!]*[^.]$", []byte(w)); m { //Yell
+	// 		if i == len(words)-1 {
+	// 			return "Whoa, chill out!"
+	// 		}
+	// 	}
+	// }
+	w := words[len(words)-1] //w is the final word
+	// m, _ := regexp.Match("^[A-Z]{2,}[?]$", []byte(w)); m { //Yell a Question
+	// 	return "Calm down, I know what I'm doing!"
+	// } else
+	if m, _ := regexp.Match("[?]$", []byte(w)); m { //Question
+		return "Sure."
+	} else if m, _ := regexp.Match("^[A-Z]{2,}.*[!]*[^.]$", []byte(w)); m { //Yell
+		return "Whoa, chill out!"
+	}
+
+	return "Whatever."
+}
