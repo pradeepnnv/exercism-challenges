9efe5d35a4cec54f3704625ce99695758d51120c
pradeepnnv <pradeepnnv@gmail.com>
2022-07-04 22:50:51 -0500

    two-fer

--- bash/two-fer/two_fer.sh
+++ bash/two-fer/two_fer.sh
@@ -1,7 +1,6 @@
-#!/usr/bin/env bash
-echo $@
+#!/bin/bash
+IFS='' # This is need to ensure bash does not split it's arguments if they have spaces. Refer to https://www.gnu.org/software/bash/manual/bash.html#Word-Splitting.
 name="$1"
-
 if [ -z "$name" ]
 then
   name='you'
