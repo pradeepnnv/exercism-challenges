4505d1f5fb50c2b66fa9a39838d8f62cbefc7e7e
pradeepnnv <pradeepnnv@gmail.com>
2022-07-24 23:47:11 -0500

    changes to bob
    Signed-off-by: pradeepnnv <pradeepnnv@gmail.com>

--- go/bob/bob.go
+++ go/bob/bob.go
@@ -7,25 +7,29 @@ package bob
 
 import (
 	"fmt"
-	"regexp"
+	"strings"
 )
 
 // Hey should have a comment documenting it.
 func Hey(remark string) string {
 	r := "Whatever."
-	if regexp.MustCompile(`[A-Z0-9_][?]$`).MatchString(remark) { //yell a question at him
-		fmt.Println("Yelled Question")
-		r = "Calm down, I know what I'm doing!"
-	} else if regexp.MustCompile(`[A-Z0-9][!]?$`).MatchString(remark) { // if you YELL AT HIM (in all capitals).
-		fmt.Println("Yelled statement")
-		r = "Whoa, chill out!"
+	// if regexp.MustCompile(`[A-Z]\?$`).MatchString(remark) { //yell a question at him
+	// 	fmt.Println("Yelled Question")
+	// 	r = "Calm down, I know what I'm doing!"
+	// } else if regexp.MustCompile(`[A-Z0-9][!]?$`).MatchString(remark) { // if you YELL AT HIM (in all capitals).
+	// 	fmt.Println("Yelled statement")
+	// 	r = "Whoa, chill out!"
 
-	} else if regexp.MustCompile(`\w[?]$`).MatchString(remark) { //check if it's a question (not yelled)
-		fmt.Println("Normal Question")
-		r = "Sure."
-	} else if !regexp.MustCompile(`[^a-zA-Z0-9_]+$`).MatchString(remark) { // if you address him without actually saying anything.
-		fmt.Println("Say nothing")
-		r = "Fine. Be that way!"
+	// } else if regexp.MustCompile(`\w[?]$`).MatchString(remark) { //check if it's a question (not yelled)
+	// 	fmt.Println("Normal Question")
+	// 	r = "Sure."
+	// } else if !regexp.MustCompile(`[^a-zA-Z0-9_]+$`).MatchString(remark) { // if you address him without actually saying anything.
+	// 	fmt.Println("Say nothing")
+	// 	r = "Fine. Be that way!"
+	// }
+	remark = strings.Trim(remark, " ")
+	if len(remark) != 1 {
+		fmt.Println("ha ha")
 	}
 	return r
 }
